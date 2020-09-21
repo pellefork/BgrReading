3faece51021f6ca40afa08ce2b6d88f9f8a582d2
per.fork <per.fork@valtech.com>
2020-09-21 09:18:00 +0200

    Created stubs for session list and maps activities

---
+++ app/src/main/java/se/fork/bgrreading/SessionListActivity.kt
@@ -0,0 +1,11 @@
+package se.fork.bgrreading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SessionListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_session_list)
+    }
+}
