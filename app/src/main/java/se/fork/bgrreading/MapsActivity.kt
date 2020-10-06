502f82ca914236f6c54d06e6dd460958294d7dc1
per.fork <per.fork@valtech.com>
2020-10-06 15:29:18 +0200

    Title showing in maps activity

--- app/src/main/java/se/fork/bgrreading/MapsActivity.kt
+++ app/src/main/java/se/fork/bgrreading/MapsActivity.kt
@@ -56,6 +56,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         setupGauges()
 
